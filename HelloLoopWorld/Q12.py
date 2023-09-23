0421aebf0b367481ecf624c12cb05ed28afe9d31
Ntheus <matheusoliveira.7313@gmail.com>
2023-09-23 14:31:50 -0300

    9 e 12 finalizadas

--- HelloLoopWorld/Q12.py
+++ HelloLoopWorld/Q12.py
@@ -2,16 +2,24 @@ tamanhoDaArea = int(input("Digite o tamanho da area"))
 print("precisa de tanto litros", tamanhoDaArea/6)
 custoAreaLataode18 = 80/(6*18)
 custoAreaLatinha = 25/(6*3.6)
-print("custoAreaLataode18 = ", (6*18)/80)
-print("custoAreaLatinha = ",(6*3.6)/25)
-litrosNecessarios = tamanhoDaArea/6
+litrosNecessarios = (tamanhoDaArea/6) * 1.1
 latasdedezoito = litrosNecessarios // 18
-print(litrosNecessarios)
+print(litrosNecessarios,"L")
 qtdSobrada = litrosNecessarios - (latasdedezoito * 18)
+print(qtdSobrada)
 
-if((18 - qtdSobrada) > (10.8 - qtdSobrada)):
+if((qtdSobrada > 10.8) or qtdSobrada == 0):
     print("Compra so latas de 18")
-    print(latasdedezoito + 1)
+    if(qtdSobrada != 0):
+        print(latasdedezoito + 1)
+    print("Custo total:",latasdedezoito * 80 )
+elif (litrosNecessarios > 10.8):
+    print("Misture")
+    print(latasdedezoito, "Latas de 18")
+    print(qtdSobrada // 3.6 + 1, "Latas de 3.6")
+    print("Custo total:",latasdedezoito * 80 +( qtdSobrada // 3.6 + 1) * 25)
+    
 else:
-    print(qtdSobrada // 3,6 + 1)
-
+    print("Compre somente de 3.6 litros")
+    print(qtdSobrada // 3.6 + 1, "Latas de 3.6")
+    print("Custo total: ", (qtdSobrada // 3.6 + 1) * 25)
--- HelloLoopWorld/Q9.py
+++ HelloLoopWorld/Q9.py
@@ -1,6 +1,29 @@
-consumidorTipoUm = []
-consumidorTipoDois = []
-
-hash = {"1":[], "2":"dois", "3":"tres" }
-hash.update("1" [1,2])
-print(hash["1"])
+map = {1: [],2: [],3: []}
+costMap = {1: 0.3,2: 0.5,3: 0.7}
+while(True):
+    numeroDoConsumidor = int(input("Informe o numero do consumidor"))
+    if(numeroDoConsumidor == 0):
+        break
+    kwPorHoraConsumidor = int(input("Informe os Kw/h consumidos"))
+    tipoDeConsumidor = int(input("Informe o tipo do consumidor"))
+    if(tipoDeConsumidor > 0 and tipoDeConsumidor < 4):
+        listaDoTipo = map.get(tipoDeConsumidor)
+        listaDoTipo.append(kwPorHoraConsumidor)
+    else:
+        print("tipo invalido")
+print(map)
+custoTotal = 0
+mediaTipoUmEDois = 0
+for i in range (1,4):
+    vetor = map.get(i)
+    for consumo in vetor:
+        custo = consumo * costMap.get(i)
+        custoTotal = custoTotal + custo
+        print("Custo do consumidor:",custo)
+        if(i<3):
+            mediaTipoUmEDois = consumo + mediaTipoUmEDois
+        continue
+    i = i + 1
+    continue
+print("Custo total:",custoTotal)
+print("Media tipo 1 e 2:", mediaTipoUmEDois/(int(len(map.get(1)))+int(len(map.get(2)))))
